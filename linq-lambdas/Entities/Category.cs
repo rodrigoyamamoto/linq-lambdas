124b79e72f63b5f91587e5e50a88fbddff9dcd8d
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2018-10-21 02:53:39 -0300

    Using linq + lambdas

---
+++ linq-lambdas/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace linq_lambdas.Entities {
+    class Category {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
